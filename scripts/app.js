ddf96cc5a10ff4440fd7e47f56291e5b5301f1ed
abedassoum <abd.assoum@outlook.com>
2023-05-09 11:23:01 +0200

    Login function virker

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,57 @@
 // main script file
+
+window.addEventListener("load", getUserByUsername);
+
+async function getUserByUsername(username) {
+  const response = await fetch(
+    "https://delfinen-database-default-rtdb.europe-west1.firebasedatabase.app/users.json"
+  );
+  const users = await response.json();
+  console.log("users");
+  console.log(users);
+  if (users) {
+    const keys = Object.keys(users);
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      if (users[key].username === username) {
+        return users[key];
+      }
+    }
+  }
+
+  return null;
+}
+
+function redirectToUserPage(userType) {
+  if (userType === "admin") {
+    window.location.href = "formand.html";
+  } else if (userType === "kasser") {
+    window.location.href = "kasser.html";
+  } else if (userType === "træner") {
+    window.location.href = "træner.html";
+  } else {
+    alert("Invalid userType");
+  }
+}
+
+document
+  .querySelector("#submit")
+  .addEventListener("click", async function (event) {
+    event.preventDefault();
+
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+    console.log(username);
+    console.log(password);
+
+    const user = await getUserByUsername(username);
+    if (user) {
+      if (user.password === password) {
+        redirectToUserPage(user.usertype);
+      } else {
+        alert("Invalid password");
+      }
+    } else {
+      alert("Invalid username");
+    }
+  });
