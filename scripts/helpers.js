3bae27c04caa4b824091bd25be24e24c8fcf7fda
Blue-Grizzly <123905990+Blue-Grizzly@users.noreply.github.com>
2023-05-09 10:08:29 +0200

    getMember

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,1 +1,15 @@
 //helping functions
+
+
+function prepareData(dataObject) {
+    const memberArray = [];
+    for (const key in dataObject) {
+      const memberObject = dataObject[key];
+      memberObject.id = key;
+      memberArray.push(memberObject);
+    }
+    console.log(memberArray);
+    return memberArray;
+  }
+  
+  export {prepareData};
--- scripts/rest-service.js
+++ scripts/rest-service.js
@@ -1,3 +1,15 @@
 //rest functions
+import{prepareData} from "./helpers.js";
+
 
 const endpoint = "https://delfinen-database-default-rtdb.europe-west1.firebasedatabase.app/";
+
+async function getMember() {
+    const response = await fetch(`${endpoint}/medlemmer.json`);
+    const data = await response.json();
+    return prepareData(data);
+  }
+  
+
+
+export {getMember};
