8c7c2f137a1d289a9e0f5512e34b9e98e9ab7e54
NikolajChristianMoeller <nikolajchristianmoeller@gmail.com>
2023-05-09 12:23:53 +0200

    Formandsliste oprettelse

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,1 +1,10 @@
-//helping functions
+function prepareData(dataObject) {
+  const medlemArray = [];
+  for (const key in dataObject) {
+    const medlemObject = dataObject[key];
+    characterObject.id = key;
+    medlemArray.push(medlemObject);
+  }
+  console.log(medlemArray);
+  return medlemArray;
+}
--- scripts/rest-service.js
+++ scripts/rest-service.js
@@ -1,1 +1,9 @@
-//rest functions
+
+
+const endpoint = "https://delfinen-database-default-rtdb.europe-west1.firebasedatabase.app/";
+
+async function getCharacters() {
+  const response = await fetch(`${endpoint}/medlemmer.json`);
+  const data = await response.json();
+  return prepareData(data);
+}
